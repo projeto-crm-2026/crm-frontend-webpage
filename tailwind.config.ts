d86632576640ba053290f9d6bf7cb6972e43a020
unknown <vitorgabrielsbo1460@gmail.com>
2026-01-21 23:04:09 -0300

    feat: install crm-project-ui

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+import preset from "crm-project-ui/tailwind-preset"
+
+const tailwindConfig = {
+  presets: [preset],
+  content: [
+    "./src/**/*.{ts,tsx}",
+    "./node_modules/crm-project-ui/dist/**/*.{js,ts}"
+  ],
+}
+
+export default tailwindConfig
