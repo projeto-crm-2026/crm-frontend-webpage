787e7e1b4ba02e37f3e7205e511505c82f31d356
unknown <vitorgabrielsbo1460@gmail.com>
2026-02-03 16:54:27 -0300

    feat: add new css classes and animations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,8 +3,10 @@ import preset from 'crm-project-ui/tailwind-preset'
 const tailwindConfig = {
   presets: [preset],
   content: [
-    './src/**/*.{ts,tsx}',
-    './node_modules/crm-project-ui/dist/**/*.{js,ts}'
+    './node_modules/crm-project-ui/dist/**/*.{js,ts}',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './**/*.{js,ts,jsx,tsx}',
+    './node_modules/crm-project-ui/**/*.{js,ts,jsx,tsx}'
   ]
 }
 
