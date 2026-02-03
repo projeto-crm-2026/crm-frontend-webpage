82f7ddf5a8aceb91e538f0f27229b4088510cbc2
unknown <vitorgabrielsbo1460@gmail.com>
2026-02-03 16:55:06 -0300

    feat: add new fira code font size

---
+++ src/constants/font/index.ts
@@ -0,0 +1,8 @@
+import { Fira_Code } from 'next/font/google'
+
+export const FONT_FAMILY = Fira_Code({
+  weight: ['300', '400', '500', '600', '700'],
+  subsets: ['latin'],
+  variable: '--font-tasa',
+  display: 'swap'
+})
